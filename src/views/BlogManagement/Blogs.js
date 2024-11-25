08a3a0245530bdd75d84a303ad243b702ed037e6
anurag721721 <anurag.yadav@aayaninfotech.com>
2024-11-25 15:28:24 +0530

    ques. and ans.

--- src/views/BlogManagement/Blogs.js
+++ src/views/BlogManagement/Blogs.js
@@ -1,167 +1,209 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 import {
   CButton,
   CForm,
   CFormInput,
   CFormTextarea,
-  CFormLabel,
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CRow,
-  CCol,
   CInputGroup,
   CInputGroupText,
+  CRow,
+  CCol,
   CModal,
   CModalBody,
   CModalFooter,
   CModalHeader,
+  CCard,
+  CCardBody,
+  CCardHeader,
   CTable,
   CTableHead,
   CTableRow,
   CTableHeaderCell,
   CTableBody,
   CTableDataCell,
-  CPagination,
-  CPaginationItem,
-} from "@coreui/react";
-import { cilImage, cilPencil, cilTrash } from "@coreui/icons";
-import CIcon from "@coreui/icons-react";
-import axios from "axios";
+} from '@coreui/react'
+import { cilImage, cilPencil, cilTrash } from '@coreui/icons'
+import CIcon from '@coreui/icons-react'
+import axios from 'axios'
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([])
+  const [modalOpen, setModalOpen] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [blogsPerPage] = useState(5)
   const [formData, setFormData] = useState({
-    blogForm: { title: "", description: "", images: null },
+    blogForm: {
+      title: '',
+      description: '',
+      image: null,
+      video: null, 
+    },
+    qaList: [],
     isEditing: false,
     editingIndex: null,
-  });
-  const [blogs, setBlogs] = useState([]);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [blogsPerPage] = useState(5);
+  })
+
+  const handleQAChange = (index, field, value) => {
+    const updatedQA = [...formData.qaList]
+    updatedQA[index][field] = value
+    setFormData({ ...formData, qaList: updatedQA })
+  }
+
+  const addQAField = () => {
+    setFormData({
+      ...formData,
+      qaList: [...formData.qaList, { question: '', answer: '' }],
+    })
+  }
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData((prevState) => ({
       ...prevState,
       blogForm: { ...prevState.blogForm, [name]: value },
-    }));
-  };
+    }))
+  }
 
   const handleFileChange = (e) => {
-    const file = e.target.files[0];
+    const { name, files } = e.target
+    const file = files[0]
     setFormData((prevState) => ({
       ...prevState,
-      blogForm: { ...prevState.blogForm, images: file },
-    }));
-  };
+      blogForm: { ...prevState.blogForm, [name]: file },
+    }))
+  }
 
   const getBlogs = async () => {
     try {
-      const res = await axios.get("http://localhost:8000/blogs/get-blogs");
-      setBlogs(res.data.updatedBlogList);
+      const res = await axios.get('http://localhost:8000/blogs/get-blogs')
+      setBlogs(res.data.updatedBlogList)
     } catch (error) {
-      console.error("Error while fetching blogs:", error);
+      console.error('Error while fetching blogs:', error)
     }
-  };
+  }
 
   useEffect(() => {
-    getBlogs();
-  }, []);
+    getBlogs()
+  }, [])
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { blogForm, isEditing, editingIndex } = formData;
+    e.preventDefault()
+    const { blogForm, qaList, isEditing, editingIndex } = formData
 
-    const blogSubmitData = new FormData();
-    blogSubmitData.append("title", blogForm.title);
-    blogSubmitData.append("description", blogForm.description);
-    if (blogForm.images) blogSubmitData.append("images", blogForm.images);
+    const blogSubmitData = new FormData()
+
+    blogSubmitData.append('title', blogForm.title)
+    blogSubmitData.append('description', blogForm.description)
+
+    if (blogForm.image) blogSubmitData.append('image', blogForm.image)
+    if (blogForm.video) blogSubmitData.append('video', blogForm.video)
+
+    qaList.forEach((qa, index) => {
+      blogSubmitData.append(`qaList[${index}][question]`, qa.question)
+      blogSubmitData.append(`qaList[${index}][answer]`, qa.answer)
+    })
 
     try {
-      let response;
+      let response
       if (isEditing) {
-        const blogId = blogs[editingIndex]._id;
-        response = await axios.put(`/api/blogs/${blogId}`, blogSubmitData);
+        const blogId = blogs[editingIndex]._id
+        response = await axios.put(`http://localhost:8000/blogs/update/${blogId}`, blogSubmitData)
       } else {
-        response = await axios.post("http://localhost:8000/blogs/createBlog", blogSubmitData);
+        response = await axios.post('http://localhost:8000/blogs/createBlog', blogSubmitData)
       }
 
       if (response.status === 200 || response.status === 201) {
-        getBlogs(); // Refresh blogs after submitting
+        getBlogs() // Fetch updated blog list
         setFormData({
-          blogForm: { title: "", description: "", images: null },
+          blogForm: { title: '', description: '', image: null, video: null },
+          qaList: [],
           isEditing: false,
           editingIndex: null,
-        });
-        setModalOpen(false); // Close modal after submit
-        document.querySelector("input[type='file']").value = ""; // Reset file input
+        })
+        setModalOpen(false)
+        document.querySelector("input[type='file']").value = null // Clear file input
       }
     } catch (error) {
-      console.error("Error submitting the blog:", error);
+      console.error('Error submitting the blog:', error)
     }
-  };
+  }
 
+  // Edit a blog
   const handleEdit = (blog) => {
     setFormData({
       blogForm: blog,
       isEditing: true,
       editingIndex: blogs.findIndex((b) => b._id === blog._id),
-    });
-    setModalOpen(true); // Open the modal for editing
-  };
+      qaList: blog.qaList || [],
+    })
+    setModalOpen(true)
+  }
 
+  // Delete a blog
   const handleDelete = async (blogId) => {
     try {
-      const response = await axios.delete(`http://localhost:8000/blogs/delete/${blogId}`);
+      const response = await axios.delete(`http://localhost:8000/blogs/delete/${blogId}`)
       if (response.status === 200) {
-        setBlogs(blogs.filter((blog) => blog._id !== blogId));
+        setBlogs(blogs.filter((blog) => blog._id !== blogId))
       }
     } catch (error) {
-      console.error("Error deleting the blog:", error);
+      console.error('Error deleting the blog:', error)
     }
-  };
+  }
 
-  // Pagination Logic
-  const indexOfLastBlog = currentPage * blogsPerPage;
-  const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
-  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
-  const totalPages = Math.ceil(blogs.length / blogsPerPage);
+  // Pagination logic
+  const indexOfLastBlog = currentPage * blogsPerPage
+  const indexOfFirstBlog = indexOfLastBlog - blogsPerPage
+  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog)
 
   return (
     <>
-      <CButton
-        color="primary"
-        onClick={() => {
-          setFormData({
-            blogForm: { title: "", description: "", images: null },
-            isEditing: false, // Ensure it's set to false when creating a new blog
-            editingIndex: null,
-          });
-          setModalOpen(true); // Open modal for creating a new blog
-        }}
-      >
-        Create Blog
-      </CButton>
-
       <CModal visible={modalOpen} onClose={() => setModalOpen(false)}>
         <CModalHeader closeButton>
-          <h4>{formData.isEditing ? "Edit Blog" : "Create Blog"}</h4>
+          <h4>{formData.isEditing ? 'Edit Blog' : 'Create Blog'}</h4>
         </CModalHeader>
         <CModalBody>
           <CForm onSubmit={handleSubmit}>
+            {/* Image Input */}
             <CRow className="mb-3">
               <CCol md={6}>
+                <h5>Image </h5>
                 <CInputGroup>
                   <CInputGroupText>
                     <CIcon icon={cilImage} />
                   </CInputGroupText>
-                  <CFormInput type="file" name="image" accept="image/*" onChange={handleFileChange} />
+                  <CFormInput
+                    type="file"
+                    name="image"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                  />
                 </CInputGroup>
               </CCol>
             </CRow>
+
+            {/* Video Input */}
             <CRow className="mb-3">
               <CCol md={6}>
+                <h5>Video </h5>
+                <CInputGroup>
+                  <CInputGroupText>
+                    <CIcon icon={cilImage} /> {/* You can use a video icon here */}
+                  </CInputGroupText>
+                  <CFormInput
+                    type="file"
+                    name="video"
+                    accept="video/*"
+                    onChange={handleFileChange}
+                  />
+                </CInputGroup>
+              </CCol>
+            </CRow>
+
+            {/* Title Input */}
+            <CRow className="mb-3">
+              <CCol md={6}>
+                <h5>Title</h5>
                 <CInputGroup>
                   <CInputGroupText>
                     <CIcon icon={cilPencil} />
@@ -177,9 +219,11 @@ const Blogs = () => {
                 </CInputGroup>
               </CCol>
             </CRow>
+
+            {/* Description Input */}
             <CRow className="mb-3">
               <CCol md={12}>
-                <CFormLabel>Description</CFormLabel>
+                <h5>Description</h5>
                 <CFormTextarea
                   name="description"
                   value={formData.blogForm.description}
@@ -187,12 +231,40 @@ const Blogs = () => {
                   rows="4"
                   placeholder="Enter Description"
                   required
-                ></CFormTextarea>
+                />
               </CCol>
             </CRow>
+
+            {/* Q&A Section */}
+            <CRow className="mb-3">
+              <CCol md={12}>
+                <h5>Questions and Answers</h5>
+                {formData.qaList.map((qa, index) => (
+                  <div key={index} className="mb-3">
+                    <CFormInput
+                      type="text"
+                      placeholder={`Question ${index + 1}`}
+                      value={qa.question}
+                      onChange={(e) => handleQAChange(index, 'question', e.target.value)}
+                      className="mb-2"
+                    />
+                    <CFormTextarea
+                      placeholder={`Answer ${index + 1}`}
+                      value={qa.answer}
+                      onChange={(e) => handleQAChange(index, 'answer', e.target.value)}
+                    />
+                  </div>
+                ))}
+                <CButton color="secondary" onClick={addQAField} className="mt-2">
+                  Add Question and Answer
+                </CButton>
+              </CCol>
+            </CRow>
+
+            {/* Modal Footer */}
             <CModalFooter>
               <CButton type="submit" color="primary">
-                {formData.isEditing ? "Update Blog" : "Create Blog"}
+                {formData.isEditing ? 'Update Blog' : 'Create Blog'}
               </CButton>
               <CButton color="secondary" onClick={() => setModalOpen(false)}>
                 Close
@@ -202,84 +274,69 @@ const Blogs = () => {
         </CModalBody>
       </CModal>
 
+      {/* Blogs List */}
       <CCard>
-        <CCardHeader>
+        <CCardHeader className="d-flex justify-content-between align-items-center">
           <h4>Blogs List</h4>
+          <CButton
+            color="primary"
+            onClick={() => {
+              setFormData({
+                blogForm: { title: '', description: '', image: null, video: null },
+                qaList: [],
+                isEditing: false,
+                editingIndex: null,
+              })
+              setModalOpen(true)
+            }}
+          >
+            Create New Blog
+          </CButton>
         </CCardHeader>
         <CCardBody>
-          {blogs.length === 0 ? (
-            <p>No blogs available. Start by creating one!</p>
-          ) : (
-            <CTable hover striped>
-              <CTableHead>
-                <CTableRow>
-                  <CTableHeaderCell>Image</CTableHeaderCell>
-                  <CTableHeaderCell>Title</CTableHeaderCell>
-                  <CTableHeaderCell>Description</CTableHeaderCell>
-                  <CTableHeaderCell>Actions</CTableHeaderCell>
+          <CTable hover>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell>Image</CTableHeaderCell>
+                <CTableHeaderCell>Title</CTableHeaderCell>
+                <CTableHeaderCell>Description</CTableHeaderCell>
+                <CTableHeaderCell>Actions</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              {currentBlogs.map((blog) => (
+                <CTableRow key={blog._id}>
+                  <CTableDataCell>
+                    {blog.image && (
+                      <img
+                        src={blog.image[0]}
+                        alt={blog.title}
+                        style={{
+                          width: '100px',
+                          height: '100px',
+                          objectFit: 'cover',
+                        }}
+                      />
+                    )}
+                  </CTableDataCell>
+                  <CTableDataCell>{blog.title}</CTableDataCell>
+                  <CTableDataCell>{blog.description}</CTableDataCell>
+                  <CTableDataCell>
+                    <CButton color="warning" onClick={() => handleEdit(blog)}>
+                      <CIcon icon={cilPencil} />
+                    </CButton>
+                    <CButton color="danger" onClick={() => handleDelete(blog._id)} className="ms-2">
+                      <CIcon icon={cilTrash} />
+                    </CButton>
+                  </CTableDataCell>
                 </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                {currentBlogs.map((blog) => (
-                  <CTableRow key={blog._id}>
-                    <CTableDataCell>
-                      {blog.image && (
-                        <img
-                          src={blog.image[0]}
-                          alt={blog.title}
-                          style={{
-                            width: "100px",
-                            height: "100px",
-                            objectFit: "cover",
-                          }}
-                        />
-                      )}
-                    </CTableDataCell>
-                    <CTableDataCell>{blog.title}</CTableDataCell>
-                    <CTableDataCell>{blog.description}</CTableDataCell>
-                    <CTableDataCell>
-                      <CButton color="info" size="sm" onClick={() => handleEdit(blog)}>
-                        <CIcon icon={cilPencil} /> Edit
-                      </CButton>{" "}
-                      <CButton color="danger" size="sm" onClick={() => handleDelete(blog._id)}>
-                        <CIcon icon={cilTrash} /> Delete
-                      </CButton>
-                    </CTableDataCell>
-                  </CTableRow>
-                ))}
-              </CTableBody>
-            </CTable>
-          )}
-          {/* <CPagination aria-label="Page navigation">
-            <CPaginationItem
-              onClick={() => setCurrentPage(1)}
-              disabled={currentPage === 1}
-            >
-              First
-            </CPaginationItem>
-            <CPaginationItem
-              onClick={() => setCurrentPage(currentPage - 1)}
-              disabled={currentPage === 1}
-            >
-              Previous
-            </CPaginationItem>
-            <CPaginationItem
-              onClick={() => setCurrentPage(currentPage + 1)}
-              disabled={currentPage === totalPages}
-            >
-              Next
-            </CPaginationItem>
-            <CPaginationItem
-              onClick={() => setCurrentPage(totalPages)}
-              disabled={currentPage === totalPages}
-            >
-              Last
-            </CPaginationItem>
-          </CPagination> */}
+              ))}
+            </CTableBody>
+          </CTable>
         </CCardBody>
       </CCard>
     </>
-  );
-};
+  )
+}
 
-export default Blogs;
+export default Blogs
